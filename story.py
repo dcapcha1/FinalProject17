a0295fadf29c2a6fdcb421fbaf9c0179cfa1078f
dcapcha1 <26dcapcha@gmail.com>
2018-01-03 15:33:19 +0000

    story.py1

--- story.py
+++ story.py
@@ -5,6 +5,7 @@ ans = print(input('It is pitch-black except for the small circle of light in the
 
 if ans == 'y':
     print('The light is getting bigger, until you are completely enveloped in the light.')
-    
+
 if ans == 'n':
     print('Come on, you know you wanna go toward the light. That is the only way the story will progress.')
+    sdds
