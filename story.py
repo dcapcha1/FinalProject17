1886ab910f9820027077e770647a04383532b629
dcapcha1 <33286711+dcapcha1@users.noreply.github.com>
2018-01-03 10:22:47 -0500

    Create story.py

---
+++ story.py
@@ -0,0 +1,10 @@
+# story of life
+print('Some useful info: type "y" for yes and "n" for no in response to yes or no questions')
+
+ans = print(input('It is pitch-black except for the small circle of light in the distance. Go toward the light? '))
+
+if ans == 'y':
+    print('The light is getting bigger, until you are completely enveloped in the light.')
+    
+if ans == 'n':
+    print('Come on, you know you wanna go toward the light. That is the only way the story will progress.')
